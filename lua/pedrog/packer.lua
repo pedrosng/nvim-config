eb31c6724fa7be913dfd20d602bdff0419ce3ec3
pedro goncalves <pedro.sng@gmail.com>
2023-11-17 11:13:04 +0000

    updates harpoon key map

--- lua/pedrog/packer.lua
+++ lua/pedrog/packer.lua
@@ -7,11 +7,12 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.4',
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
 -- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'}, {'nvim-lua/popup.nvim'} }
-}
+    requires = { {'nvim-lua/plenary.nvim'}, {'nvim-lua/popup.nvim'} }
+  }
+
   use({ 
     'rose-pine/neovim',
     as = 'rose-pine',
